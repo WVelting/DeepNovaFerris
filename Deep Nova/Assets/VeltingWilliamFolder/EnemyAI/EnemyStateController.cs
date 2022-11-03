9edaf3b921215f1da0855628d29b863312b31e35
Bill <wvelting@gmail.com>
2022-11-03 16:09:01 -0400

    More work on Enemy AI
    Enemy AI now has a wander function. There is some ironing needed to be done on speed of moving through and turning, but it's starting to shape up

--- Deep Nova/Assets/VeltingWilliamFolder/EnemyAI/EnemyStateController.cs
+++ Deep Nova/Assets/VeltingWilliamFolder/EnemyAI/EnemyStateController.cs
@@ -7,17 +7,23 @@ public class EnemyStateController : MonoBehaviour
 
     private EnemyShip enemy; //enemy ship
     private GameObject player; //player ship
+
+    public Vector3 randomPath;
+    public Vector3 originPosition;
     void Start()
     {
         enemy = GetComponent<EnemyShip>(); //assigns enemy ship to enemy
         player = GameObject.FindGameObjectWithTag("Player"); //assigns player ship to player
+        originPosition = transform.position;
+        CalcNewPath();
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        SteerToPlayer();
+        //SteerToPlayer();
+        Wander();
     }
 
     //lerp function for animation purposes
@@ -37,6 +43,45 @@ public class EnemyStateController : MonoBehaviour
         return (b - a) * p + a;
     }
 
+    public static Quaternion Lerp(Quaternion a, Quaternion b, float p)
+    {
+        b = WrapQuaternion(a, b);
+        
+        Quaternion rot = Quaternion.identity;
+
+        rot.x = Lerp(a.x, b.x, p);
+        rot.y = Lerp(a.y, b.y, p);
+        rot.z = Lerp(a.z, b.z, p);
+        rot.w = Lerp(a.w, b.w, p);
+
+        return rot;
+    }
+
+    public static float AngleWrapDegrees(float baseAngle, float angleToBeWrapped)
+    {
+        while (baseAngle > angleToBeWrapped + 180) angleToBeWrapped += 360;
+        while(baseAngle < angleToBeWrapped - 180) angleToBeWrapped -= 360;
+
+        return angleToBeWrapped;
+
+    }
+
+    public static Quaternion WrapQuaternion(Quaternion baseAngle, Quaternion angleToBeWrapped)
+    {
+        float alignment = Quaternion.Dot(baseAngle, angleToBeWrapped);
+
+        if(alignment < 0)
+        {
+            angleToBeWrapped.x *= -1;
+            angleToBeWrapped.y *= -1;
+            angleToBeWrapped.z *= -1;
+            angleToBeWrapped.w *= -1;
+
+        }
+
+        return angleToBeWrapped;
+    }
+
     //has enemy ship look toward player and then move toward player once a set distance has been reached between the two players
     void SteerToPlayer()
     {
@@ -51,6 +96,33 @@ public class EnemyStateController : MonoBehaviour
         {
             enemy.transform.position = Lerp(transform.position, player.transform.position, .005f);
         }
+    }
+
+    void Wander()
+    {
+        if((transform.position - randomPath).magnitude >= 50)
+        {
+            if((transform.position - originPosition).magnitude <= 150)
+            {
+                Quaternion lookForward = Quaternion.LookRotation((randomPath - transform.position), Vector3.up);
+                transform.position = Lerp(transform.position, randomPath, .001f);
+                transform.rotation = Lerp(transform.rotation, lookForward, .005f);
+            }
+
+            else randomPath = originPosition;
+        }
+        else
+        {
+            CalcNewPath();
+        }
+    }
+
+    Vector3 CalcNewPath()
+    {
+        
+        int wanderLength = Random.Range(100, 200);
+        randomPath = new Vector3(originPosition.x+Random.Range(-wanderLength, wanderLength),originPosition.y+Random.Range(-wanderLength, wanderLength),originPosition.z+Random.Range(-wanderLength, wanderLength));
+        return randomPath;
     }
 
 }
